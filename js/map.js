87378a4e3877d06b8c050012a8db9523c0a1d751
mo3az96 <mo3az.abdellatif@gmail.com>
2023-12-18 18:42:48 +0200

    contact page

---
+++ js/map.js
@@ -0,0 +1,10 @@
+function initMap() {
+  var lat = $("#map").data("lat");
+  var lng = $("#map").data("lng");
+  const myLatlng = { lat: lat, lng: lng };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 17,
+    center: myLatlng,
+    draggable: false,
+  });
+}
